d33e06d887636d19a6a281cb2c914dadfcf626ac
Paul Ventura <j.paul.ventura@gmail.com>
2024-06-02 14:09:51 -0500

    Authentication

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,33 @@ import { Suspense } from "react";
 import './App.css'
 
 import { Outlet } from "react-router-dom";
+import { AuthProvider } from "./auth/userContext";
+
+import { Routes, Route } from 'react-router-dom';
+import PrivateRoute from './auth/PrivateRoute';
+
+import LoginForm from "./components/login/LoginForm";
+import HomePage from "./routes/HomePage"
 
 function App() {
   return (
-    <>
-    <div>
-      <h1>Willkommen zurück, Leute.</h1>
-      <Suspense fallback={<div>loading...</div>}>
-        <Outlet />
-      </Suspense>
-    </div>
-    </>
+    <AuthProvider>
+      <div>
+        <h1>Willkommen zurück, Leute.</h1>
+        <Suspense fallback={<div>loading...</div>}>
+          <Outlet />
+          <Routes>
+            {/* Public routes */}
+            <Route path="/login" element={<LoginForm />} />
+
+            {/* Private route using PrivateRoute component */}
+            <Route path="/" element={<PrivateRoute />}>
+              <Route path="/account" element={<HomePage />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </div>
+    </AuthProvider>
   )
 }
 
---
+++ src/components/login/LoginForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LoginForm() {
+  return (
+    <div>LoginForm
+      <button>Log in</button>
+      <button>Log out</button>
+    </div>
+  )
+}
+
+export default LoginForm
